712dae6f7bfa2c142ae2644e779d2016859003aa
allan-moya <allan.s.moya@gmail.com>
2021-11-20 21:10:11 -0800

    added redux & connect api to backend

--- server/index.js
+++ server/index.js
@@ -10,11 +10,10 @@ const CONNECTIONP_URL = 'mongodb+srv://reminiscence:reminiscence123@cluster0.w2b
 
 
 
-
+app.use(cors());
 app.use('/posts', postRoutes)
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
 
 
 mongoose.connect(CONNECTIONP_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`))).catch((error) => console.log(error.message))
